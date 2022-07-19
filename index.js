78361aa080cc4afc2fecc74af5d327d5aa219b66
unwxnted <bauti0926@gmail.com>
2022-07-19 16:02:56 -0300

    frontend base created

---
+++ index.js
@@ -0,0 +1,87 @@
+const app = document.querySelector('.app');
+const temp = document.querySelector('.temp');
+const dateOutput = document.querySelector('.date');
+const timeOutput = document.querySelector('.time');
+const conditionOutput = document.querySelector('.condition');
+const nameOutput = document.querySelector('.name');
+const cloudOutput = document.querySelector('.cloud');
+const humidityOutput = document.querySelector('.humidity');
+const windOutput = document.querySelector('.wind');
+const form = document.getElementById('inputForm');
+const search = document.querySelector('.search');
+const btn = document.querySelector('.submit');
+
+let cityInput = "Buenos Aires";
+
+function getDate(day, month, year){
+    const weekDay = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+
+    return weekDay[new Date(`${day}/${month}/${year}`).getDay()];
+};
+
+
+function fToC(fahrenheit) 
+{
+  let fTemp = fahrenheit;
+  let fToCel = (fTemp - 32) * 5 / 9;
+  return fToCel;
+} 
+
+function fecthWeather(){
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '7e7df22d7emsh4038f1d297ccc83p11378fjsneba088e09085',
+            'X-RapidAPI-Host': 'community-open-weather-map.p.rapidapi.com'
+        }
+    };
+    
+    fetch(`https://community-open-weather-map.p.rapidapi.com/weather?q=${cityInput}&units=imperial&mode=JSON`, options)
+        .then(response => response.json())
+        .then(response => {
+
+            console.log(response);
+            temp.innerHTML = parseInt(fToC(response.main.temp)) + "&#176;";
+            humidityOutput.innerHTML = response.main.humidity + "%";
+            nameOutput.innerHTML = response.name;
+            windOutput.innerHTML = response.wind.speed + " km/h";
+            cloudOutput.innerHTML = response.clouds.all + "%";
+            conditionOutput.innerHTML = response.weather[0].description;
+            let today = new Date();
+
+            let date = today.getFullYear()+'/'+(today.getMonth()+1)+'/'+today.getDate();
+            let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+            dateOutput.innerHTML = date;
+            timeOutput.innerHTML = time;
+
+
+
+        })
+        .catch(err => console.error(err));
+    
+}
+
+
+form.addEventListener('submit', (e) =>{
+    if(search.value.length == 0){
+        alert('Please type a city name');
+    }else{
+        cityInput = search.value;
+        fecthWeather();
+        search.value = "";
+    }
+
+    e.preventDefault();
+});
+
+
+fecthWeather();
